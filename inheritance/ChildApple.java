75fe0664425b3c95f07f3ee82380c659c41ec295
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-04-13 18:18:12 +0800

    Create ChildApple.java

---
+++ inheritance/ChildApple.java
@@ -0,0 +1,11 @@
+package inheritance;
+//subclass
+
+public class ChildApple extends ParentFruits {
+    //using the keyword extends maiinherit yung asa superclass
+    
+    void drinks(){
+        System.out.println("Apple Shake");
+        System.out.println("Apple Cider");
+    }
+}
