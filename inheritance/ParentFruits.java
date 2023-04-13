d957d88ebc0a1f44f09b0bf5494a30e474df6bc7
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-04-13 18:17:46 +0800

    Create ParentFruits.java

---
+++ inheritance/ParentFruits.java
@@ -0,0 +1,14 @@
+package inheritance;
+//superclass
+
+public class ParentFruits {
+    String color, taste;
+    int NumPcs;
+    
+    //method
+    void checkFruit(){
+        System.out.println("The Color is: " + color);
+        System.out.println("The Taste is: " + taste);
+        System.out.println("The Num of pcs: " + NumPcs);
+    }
+}
