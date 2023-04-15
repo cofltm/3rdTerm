bb44ff39b35815bdcf8e17694197ddb723c543c2
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-04-15 20:08:46 +0800

    Create Parent.java

---
+++ quiz/quiz1/Parent.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Parent {
+    String attitude, hairColor, bloodType, property;
+    
+    //methods
+    void checkInfos(){
+        System.out.println("Attitude: "+ attitude);
+        System.out.println("Hair Color: "+ hairColor);
+        System.out.println("Blood Type: "+ bloodType);
+        System.out.println("Property: "+ property);
+        System.out.println(" ");
+    }
+}
