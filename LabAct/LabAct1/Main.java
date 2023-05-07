45399ebd662f55b1e36fa3be4b4c50e347d9e767
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-05-07 11:01:19 +0800

    Create Main.java

---
+++ LabAct/LabAct1/Main.java
@@ -0,0 +1,15 @@
+package TimerCountdown;
+
+ 
+public class Main {
+ 
+    public static void main(String[] args)
+    {
+        
+        JavaTimer jt = new JavaTimer();
+        
+        jt.Timer();
+        
+    }
+ 
+}
