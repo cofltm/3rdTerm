cb045cb469c6eeebe05f316c13b26ece72b45309
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-05-11 18:00:36 +0800

    Create Car.java

---
+++ Interface/Car.java
@@ -0,0 +1,14 @@
+package InterfaceInheritance;
+
+public interface Car {
+    
+    
+    //abstract method = bawal gumawa ng body
+    public void wiper();
+    public void pass();
+    
+    
+   public void HondaStart();
+   public void HondaDrive();
+    
+}
