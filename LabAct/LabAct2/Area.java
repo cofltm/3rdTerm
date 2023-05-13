c5763f2a1c505bac56500c75f0ec8d97cf4ed296
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-05-13 15:02:22 +0800

    Create Area.java

---
+++ LabAct/LabAct2/Area.java
@@ -0,0 +1,18 @@
+package labact2;
+
+ 
+
+import java.util.Scanner;
+
+ 
+
+
+public interface Area {
+
+    static final Scanner in = new Scanner (System.in);
+
+    public void triangle();
+    public void rectangle();
+    public void circle();
+    public void trapezoid();
+}
